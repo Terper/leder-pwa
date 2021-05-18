6670917b8c72fd4a97189d7174ed2a3c4f1508df
Terper <jann.totterman@gmail.com>
2021-05-18 11:23:09 +0300

    icon styling

--- src/Components/Spinner/Spinner.tsx
+++ src/Components/Spinner/Spinner.tsx
@@ -2,5 +2,9 @@ import React, { ReactElement } from 'react';
 import './Spinner.css';
 
 export default function Spinner(): ReactElement {
-  return <div className='spinner'></div>;
+  return (
+    <div className='spinner'>
+      <i className='gg-spinner-two-alt'></i>
+    </div>
+  );
 }
